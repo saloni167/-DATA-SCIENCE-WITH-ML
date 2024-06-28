3b767990f99ce475de98193a95f872f517356408
saloni167 <salonipiplani16@gmail.com>
2024-06-28 21:48:01 +0530

    Add files via upload

--- assignment 3.py
+++ assignment 3.py
@@ -1,9 +1,9 @@
 import os
 import shutil
-os.chdir(r"C:\Users\adity\Desktop\Data_science\testing")
-Destination_File = r"C:\Users\adity\Desktop\jodhpur\images_files"
-Destination_File1 = r"C:\Users\adity\Desktop\jodhpur\images_files"
-Destination_File2 = r"C:\Users\adity\Desktop\jodhpur\text_files"
+os.chdir(r"C:\Users\Dell\Desktop\testing")
+Destination_File = r"C:\Users\Dell\Desktop\jodhpur\images_files"
+Destination_File1 = r"C:\Users\Dell\Desktop\jodhpur\images_files"
+Destination_File2 = r"C:\Users\Dell\Desktop\jodhpur\text_files"
 lst = os.listdir()
 for File in lst:
     if File.endswith('.png') or File.endswith('.jpg'):
